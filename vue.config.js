fc2e80f5b47a661f2be78c1a56361319f583766a
nevermin9 <chizzza@gmail.com>
2022-03-03 18:19:40 +0800

    styles setup complete

--- vue.config.js
+++ vue.config.js
@@ -1,4 +1,20 @@
 const { defineConfig } = require("@vue/cli-service")
+const resolve = require("path").resolve;
+
 module.exports = defineConfig({
-    transpileDependencies: true
+    transpileDependencies: true,
+    configureWebpack: {
+        resolve: {
+            alias: {
+                "@": resolve(__dirname, "./src"),
+            }
+        }
+    },
+    css: {
+        loaderOptions: {
+            sass: {
+                additionalData: "@import \"~@/styles/lib\";",
+            },
+        },
+    }
 })
